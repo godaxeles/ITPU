654ad503ec8959fbb64cbd6ea9fb3f5cd772af09
saLeeh <axeles687@gmail.com>
2024-11-14 22:45:29 +0500

    feat: clone repository

---
+++ Naming/Task1/CollectOrderService.cs
@@ -0,0 +1,30 @@
+﻿using Naming.Task1.ThirdParty;
+
+namespace Naming.Task1
+{
+    public class CollectOrderService : IOrderService
+    {
+        private readonly ICollectionService _collectionService;
+        private readonly INotificationManager _notificationManager;
+        private const int InfoNotificationLevel = 4;
+        private const int CriticalNotificationLevel = 1;
+
+        public CollectOrderService(ICollectionService collectionService, INotificationManager notificationManager)
+        {
+            _collectionService = collectionService;
+            _notificationManager = notificationManager;
+        }
+
+        public void Submit(IOrder order)
+        {
+            if (_collectionService.IsEligibleForCollect(order))
+            {
+                _notificationManager.NotifyCustomer(Message.ReadyForCollect, InfoNotificationLevel);
+            }
+            else
+            {
+                _notificationManager.NotifyCustomer(Message.ImpossibleToCollect, CriticalNotificationLevel);
+            }
+        }
+    }
+}
---
+++ Naming/Task1/DeliveryOrderService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Naming.Task1.ThirdParty;
+
+namespace Naming.Task1
+{
+    public class DeliveryOrderService : IOrderService
+    {
+        private readonly IDeliveryService _deliveryService;
+        private readonly IOrderFulfilmentService _orderFulfilmentService;
+
+        public DeliveryOrderService(IDeliveryService deliveryService, IOrderFulfilmentService orderFulfilmentService)
+        {
+            _deliveryService = deliveryService;
+            _orderFulfilmentService = orderFulfilmentService;
+        }
+
+        public void Submit(IOrder order)
+        {
+            if (_deliveryService.IsDeliverable())
+            {
+                var products = order.GetProducts();
+                _orderFulfilmentService.FulfilProducts(products);
+            }
+            else
+            {
+                throw new NotDeliverableOrderException();
+            }
+        }
+    }
+}
